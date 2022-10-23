39452b6a2011ffc2f94c1d9f4639a52aeaef2e1e
NamYeJi <dkdlelql123@naver.com>
2022-10-23 22:34:09 +0900

    종료기능구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,7 +1,24 @@
 package com.ll.exam;
 
+import java.util.Scanner;
+
 public class App {
     public void run() {
         System.out.println("-- 명언 SSG --");
+
+
+        Scanner sc = new Scanner(System.in);
+
+        outer:
+        while(true){
+
+            System.out.println("-- 명령해주세요. --");
+            String str = sc.nextLine().toString();
+
+            switch(str){
+                case "종료":
+                    break outer;
+            }
+        }
     }
 }
